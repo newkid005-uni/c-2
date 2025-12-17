ffe30f3dd45263c97a9cd7f67bf4e50d349ab879
newkid005-uni <newkid005@gmail.com>
2025-12-17 21:44:08 +0900

    2025.12.17 조건문

--- c-2/Program_1/Source.c
+++ c-2/Program_1/Source.c
@@ -70,25 +70,140 @@ void main()
 	//if문과 else if문의 조건이 다 틀리면
 	//실행되는 명령문입니다.
 
-	int integer = 0;
-
-	if (integer > 0)
-	{
-		printf("양수입니다.");
-	}
-	else if (integer < 0)
-	{
-		printf("음수입니다.");
-	}
-	else
-	{
-		printf("0입니다.");
-	}
+	//int integer = 0;
+	//
+	//if (integer > 0)
+	//{
+	//	printf("양수입니다.");
+	//}
+	//else if (integer < 0)
+	//{
+	//	printf("음수입니다.");
+	//}
+	//else
+	//{
+	//	printf("0입니다.");
+	//}
 
 	//if문에 연결된 모든 조건문의 조건이 맞을 때
 	//가장 위에 있는 조건문만 실행됩니다.
 
 #pragma endregion
+
+#pragma region switch문
+	//어떤 결과에 따라 그 결과부터 실행되는 명령문입니다.
+
+	//char grade = 'k';
+	//
+	////switch문은 해당 조건이 완료되었을 때, break문이 없으면
+	////나머지 밑에 있는 case문까지 계속 실행시키다 종료합니다.
+	//
+	//switch (grade)
+	//{
+	//case 'A':  printf("100 ~ 90\n"); break; 
+	//case 'B':  printf("89 ~ 80\n"); break;
+	//case 'C':  printf("79 ~ 70\n"); break;
+	//case 'D':  printf("69 ~ 60\n"); break;
+	//case 'F':  printf("59 이하\n"); break;
+	//default: printf("exception\n"); break;
+	//}
+
+	//switch문의 경우 조건에 해당하는 값에 따라 조건의
+	//위치로 이동합니다.
+#pragma endregion
+
+#pragma region 논리 연산자
+
+#pragma region AND 연산자
+//두 개의 조건이 다 성립될 때 실행되는 연산자입니다.
+//int a = 10, b = 10;
+//
+//if(a > 0 && b > 0)
+//{
+//	printf("참입니다.");
+//}
+//else
+//{
+//	printf("거짓입니다.");
+//}
+
+//조건문에서 하나 이상의 조건이 있다면 왼쪽부터 조건을 검사합니다.
+
+#pragma endregion
+
+#pragma region OR 연산자
+//두 개의 조건 중에서 하나라도 조건이 성립될 때 실행되는 연산자입니다.
+
+//int a = 10, b = 20;
+//
+//if (a >= 0 || b <= 0)
+//{
+//	printf("참입니다.");
+//}
+//else
+//{
+//	printf("거짓입니다.");
+//}
+
+// 조건문의 논리 표현식을 평가하는 도중에 결과가 이미 확정이 났다면, 그 이후의 평가는 생략합니다.
+
+#pragma endregion
+
+#pragma region NOT 연산자
+//하나의 조건을 반전시키는 연산자입니다.
+
+//int power = 0;
+//
+//
+//if (!power)
+//{
+//	printf("the power is on");
+//}
+//else
+//{
+//	printf("the power is off");
+//}
+
+#pragma endregion
+
+
+#pragma endregion
+
+#pragma region 사분면
+int X = 0;
+int Y = 0;
+
+if (X > 0 && Y > 0)
+{
+	printf("제 1 사분면");
+}
+else if (X < 0 && Y > 0)
+{
+	printf("제 2 사분면");
+}
+else if (X < 0 && Y < 0)
+{
+	printf("제 3 사분면");
+}
+else if (X > 0 && Y < 0)
+{
+	printf("제 4 사분면");
+}
+else if (X == 0 && Y < 0 || Y > 0)
+{
+	printf("Y 절편");
+}
+else if (Y == 0 && X < 0 || X > 0)
+{
+	printf("X 절편");
+}
+else
+{
+	printf("원점");
+}
+
+#pragma endregion
+
 
 #pragma endregion
 
