6c3d363422b535bd858d3f6d780456f38168d2cb
newkid005-uni <newkid005@gmail.com>
2026-01-16 21:44:02 +0900

    2026.01.16 색상 시스템

--- c-2/Program_1/Source.c
+++ c-2/Program_1/Source.c
@@ -1,5 +1,46 @@
 #include<stdio.h>
-#define SIZE 1000
+#include<stdlib.h>
+
+void load(const char* filename)
+{
+	FILE* file = fopen(filename, "r");
+
+	int count = 0;
+
+	int character = '\0'; //EOF의 반환값이 -1이므로 안전하게 int로 정의함
+
+	//파일 포인터를 파일 끝으로 이동시킵니다.
+	while ((character = fgetc(file)) != EOF)
+	{
+		count++;
+	}
+
+	//숙제 동적할당해오기
+
+	rewind(file);
+
+	char* buffer;
+	buffer = malloc(sizeof(char) * (count + 1));
+
+	//1번 매개변수 : 읽을 데이터를 저장할 메모리 버퍼의 포인터 변수
+	//2번 매개변수 : 각 데이터 항목의 크기
+	//3번 매개변수 : 테이터를 읽어올 항목의 수
+	//4번 매개변수 : 데이터를 읽어올 파일의 포인터 변수
+	//fread(&buffer, sizeof(char), sizeof(buffer), file);
+
+	for (int i = 0;i < count;i++)
+	{
+		buffer[i] = fgetc(file);
+	}
+
+	buffer[count] = NULL;
+
+	printf("%s", buffer);
+
+	free(buffer);
+	fclose(file);
+
+}
 
 int main()
 {
@@ -22,37 +63,10 @@ int main()
 #pragma endregion
 
 #pragma region 파일 읽기
-	FILE* file = fopen("data.txt", "r");
 
-	int count = 0;
-	char character = NULL;
-	
-	
-	
-
-	//파일 포인터를 파일 끝으로 이동시킵니다.
-	while ((character = fgetc(file)) != EOF)
-	{
-		count++;
-	}
-
-	printf("%d\n", count);
-	//숙제 동적할당해오기
-	char buffer[] = {0,};
-
-	//1번 매개변수 : 읽을 데이터를 저장할 메모리 버퍼의 포인터 변수
-	//2번 매개변수 : 각 데이터 항목의 크기
-	//3번 매개변수 : 테이터를 읽어올 항목의 수
-	//4번 매개변수 : 데이터를 읽어올 파일의 포인터 변수
-	//fread(buffer, sizeof(char), sizeof(buffer), file);
-
-
-	printf("%s", buffer);
-
-	fclose(file);
+	//load("resources/asc.txt");
 
 #pragma endregion
-
 
 
 #pragma endregion
