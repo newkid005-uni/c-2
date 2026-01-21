880bf0936c8604c4ad0c4181db4d759712eb1836
newkid005-uni <newkid005@gmail.com>
2026-01-21 21:44:23 +0900

    2026.01.21 게임 설계론

--- c-2/Program_1/Source.c
+++ c-2/Program_1/Source.c
@@ -8,51 +8,121 @@
 #define DOWN 80
 #define SIGNAL 224
 
-void position(int x, int y)
-{
-	COORD position = { x,y };
+int screenIndex;
+HANDLE screen[2];
 
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), position);
+void Initialize()
+{
+	CONSOLE_CURSOR_INFO cursor;
+
+	//화면 버퍼를 2개 생성합니다.
+	screen[0] = CreateConsoleScreenBuffer
+	(
+		GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+	);
+
+	screen[1] = CreateConsoleScreenBuffer
+	(
+		GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL
+	);
+
+	cursor.bVisible = FALSE;
+
+	SetConsoleCursorInfo(screen[0], &cursor);
+	SetConsoleCursorInfo(screen[1], &cursor);
+}
+
+void Flip()
+{
+	SetConsoleActiveScreenBuffer(screen[screenIndex]);
+	screenIndex = !screenIndex;
+}
+
+void Clear()
+{
+	COORD position = { 0,0 };
+
+	DWORD dword;
+
+	CONSOLE_SCREEN_BUFFER_INFO buffer;
+
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	GetConsoleScreenBufferInfo(console, &buffer);
+
+	int width = buffer.srWindow.Right - buffer.srWindow.Left + 1;
+	int height = buffer.srWindow.Bottom - buffer.srWindow.Top + 1;
+
+	FillConsoleOutputCharacter
+	(
+		screen[screenIndex], ' ', width * height, position, &dword
+	);
+}
+
+void Release()
+{
+	CloseHandle(screen[0]);
+	CloseHandle(screen[1]);
+}
+
+void Render(int x, int y, const char * character)
+{
+	DWORD dword;
+	COORD position = { x,y };
+	SetConsoleCursorPosition(screen[screenIndex], position);
+	WriteFile(screen[screenIndex], character, strlen(character), &dword, NULL);
 }
 
 int main()
 {
+	char key = 0;
+
 	int x = 0;
 	int y = 0;
-	position(5, 5);
+
+	Initialize();
+	CONSOLE_SCREEN_BUFFER_INFO buffer;
 	
-	printf("★");
+
+	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	GetConsoleScreenBufferInfo(console, &buffer);
+
+	int width = buffer.srWindow.Right - buffer.srWindow.Left - 2;
+	int height = buffer.srWindow.Bottom - buffer.srWindow.Top;
 
 	while (1)
 	{
-		
+		Flip();
+		Clear();
 
-		int key = _getch();
-		if (key == SIGNAL || key == 0)
+		key = _getch();
+
+		if (key == -32 || key == 0)
 		{
 			key = _getch();
 		}
 
 		switch (key)
 		{
-		//case SIGNAL:
-		//	break;
-		case UP:y--;
+		case UP: if (y > 0) { y--; }
 			break;
-		case LEFT:x -= 2;
+
+		case LEFT: if (x > 0) { x -= 2; }
 			break;
-		case DOWN:y++;
+
+		case RIGHT: if (y < width) { x += 2; }
 			break;
-		case RIGHT:x += 2;
+
+		case DOWN: if (y < height) { y++; }
 			break;
-		
-		default: printf("exeption\n");
+
+		default: printf("exception\n");
 			break;
 		}
-		system("cls");
-		position(x, y);
+
+		Render(x, y, "★");
 		
-		printf("★");
 	}
 
 	return 0;
